f6f16e6f808385183013656336ed13221c5b212b
Amanda.Santos <amanda.santos@zup.com.br>
2023-06-15 18:35:28 -0300

    Atualizando a classe Calculadora com o metodo de divisao

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -5,8 +5,11 @@ public class Calculadora {
     public int subtrairDoisNumeros(int n1, int n2){
         return n1 - n2;
     }
-
     public int multiplicarDoisNumeros(int valor1, int valor2){
         return valor1 * valor2;
+    }
+
+    public int dividirDoisNumeros(int numeroUm, int numeroDois){
+        return numeroUm / numeroDois;
     }
 }
