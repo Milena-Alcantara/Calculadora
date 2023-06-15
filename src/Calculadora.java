c48557ae607b706e3f0b73263ef3d55b137fb130
laura.chagas <laura.chagas@zup.com.br>
2023-06-15 18:21:07 -0300

    Atualizacao do codigo adicionando o metodo subtrairDoisNumeros através da feature subtracao

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -2,4 +2,7 @@ public class Calculadora {
     public static int somarDoisNumeros(int num1, int num2){
         return num1+num2;
     }
+    public int subtrairDoisNumeros(int n1, int n2){
+        return n1 - n2;
+    }
 }
