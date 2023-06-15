b5e10cda57ec19897d8118986b505fcc961ac781
Vitoria.Cerqueira <vitoria.cerqueira@zup.com.br>
2023-06-15 18:29:26 -0300

    Atualização da classe Calculadora com o metodo multiplicar

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -5,4 +5,8 @@ public class Calculadora {
     public int subtrairDoisNumeros(int n1, int n2){
         return n1 - n2;
     }
+
+    public int multiplicarDoisNumeros(int valor1, int valor2){
+        return valor1 * valor2;
+    }
 }
