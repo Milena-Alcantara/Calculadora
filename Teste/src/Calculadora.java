10e975a904e834db8b5f03f1160d51a7ef6f0d48
Milena.Lopes.Mesquita.Alcântara <milena.alcantara@zup.com.br>
2023-06-16 08:49:16 -0300

    Add Testes da Classe Calculadora, Release 1.1

---
+++ Teste/src/Calculadora.java
@@ -0,0 +1,14 @@
+public class Calculadora {
+    public int somarDoisNumeros(int num1, int num2){
+        return num1+num2;
+    }
+    public int subtrairDoisNumeros(int n1, int n2){
+        return n1 - n2;
+    }
+    public int multiplicarDoisNumeros(int valor1, int valor2){
+        return valor1 * valor2;
+    }
+    public int dividirDoisNumeros(int numeroUm, int numeroDois){
+        return numeroUm / numeroDois;
+    }
+}
